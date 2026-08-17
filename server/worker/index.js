c63652364b34fbf8d5e7d3a2f133536729468e83
Ruslan <superruslan2005@gmail.com>
2026-08-17 16:31:07 +0100

    Added debugging

--- server/worker/index.js
+++ server/worker/index.js
@@ -11,4 +11,20 @@ async function main() {
   }
 }
 
-main().catch(console.error);
+while (true) {
+  const response = await fetch(
+    "https://www.irishimmigration.ie/news-and-updates/",
+  );
+
+  console.log(response.status);
+
+  const html = await response.text();
+
+  console.log(html.length);
+
+  console.log("MAIN START", new Date().toLocaleTimeString());
+  await main().catch(console.error);
+  console.log("MAIN FINISHED", new Date().toLocaleTimeString());
+  console.log("Waiting for 15 minutes before next run...");
+  await new Promise((resolve) => setTimeout(resolve, 15 * 60 * 1000));
+}
